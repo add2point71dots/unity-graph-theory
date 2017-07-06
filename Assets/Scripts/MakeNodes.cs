4b6aa789614e92d1b5ce6e269bbd4cd2b2a659a5
add2point71dots <alcland@gmail.com>
2017-07-06 14:11:48 -0700

    can make nodes on click

---
+++ Assets/Scripts/MakeNodes.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakeNodes : MonoBehaviour {
+	public GameObject node;
+	public Transform nodeSpawn;
+
+	void Update () {
+		if (Input.GetButton("Fire1")) {
+			print(Input.mousePosition);
+            Instantiate(node, nodeSpawn.position, nodeSpawn.rotation);
+        }
+
+	}
+}
