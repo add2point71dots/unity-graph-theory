815605e6146249c3b89b12276d5c305482ea33a7
add2point71dots <alcland@gmail.com>
2017-07-06 11:31:27 -0700

    made a camera that moves via arrow keys

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public float rotateSpeed;
+	public float movementSpeed;
+
+	void Update () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 rotation = new Vector3 (0.0f, moveHorizontal, 0.0f);
+		Vector3 forwardMovement = new Vector3 (0.0f, 0.0f, moveVertical);
+
+		transform.Rotate (rotation * rotateSpeed * Time.deltaTime);
+		transform.Translate(forwardMovement * movementSpeed);
+	}
+}
