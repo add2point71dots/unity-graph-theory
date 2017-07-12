31ac489b42f94bb85dada83d6c897a0553ea53ef
add2point71dots <alcland@gmail.com>
2017-07-12 10:54:43 -0700

    can destroy nodes but not edges yet

---
+++ Assets/Scripts/Destroyer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroyer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (1)) {
+			RaycastHit hit; 
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
+			if (Physics.Raycast (ray , out hit, 100.0f)) {
+				Destroy(hit.transform.gameObject);
+				Debug.Log("I've HIT SOMETHING");
+			}
+		}
+	}
+}
--- Assets/Scripts/DrawEdge.cs
+++ Assets/Scripts/DrawEdge.cs
@@ -6,18 +6,30 @@ public class DrawEdge : MonoBehaviour {
 	public Transform start;
 	public Transform end;
 	private LineRenderer line;
+	private CapsuleCollider capsule;
+	public float LineWidth;
 
 	// Use this for initialization
 	void Start () {
 		line = GetComponent<LineRenderer>();
-		Debug.Log("IN DRAW EDGE");
-		Debug.Log("start: " + start);
-		Debug.Log("end: " + end);
+//		Debug.Log("IN DRAW EDGE");
+//		Debug.Log("start: " + start);
+//		Debug.Log("end: " + end);
+
+		capsule = gameObject.AddComponent<CapsuleCollider>();
+        capsule.radius = LineWidth / 2;
+        capsule.center = Vector3.zero;
+        capsule.isTrigger = true;
+        capsule.direction = 2; // Z-axis for easier "LookAt" orientation
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		line.SetPosition(0, start.position);
 		line.SetPosition(1, end.position);
+
+		capsule.transform.position = start.position + (end.position - start.position) / 2;
+ 		capsule.transform.LookAt(start.position);
+		capsule.height = (end.position - start.position).magnitude;
 	}
 }
