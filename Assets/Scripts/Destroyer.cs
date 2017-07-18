f768e7cbc513698beb0d4fc43b96cb08c36ba7e7
add2point71dots <alcland@gmail.com>
2017-07-18 10:59:38 -0700

    fixed some buggy things on destroying nodes and edges

--- Assets/Scripts/Destroyer.cs
+++ Assets/Scripts/Destroyer.cs
@@ -14,13 +14,13 @@ public class Destroyer : MonoBehaviour {
 		if (Input.GetMouseButtonDown (1)) {
 			RaycastHit hit; 
 			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
-
+			  
 			if (Physics.Raycast (ray , out hit, 100.0f)) {
 				GameObject hitObject = hit.transform.gameObject;
 
 				if (hitObject.tag == "Node") {
-					Debug.Log("I'm DESTROYING A NODE");
 					NodeController nodeController = hitObject.GetComponent<NodeController>();
+
 					for (int i = 0; i < nodeController.connectedEdges.Count; i++) {
 						GameObject edge = nodeController.connectedEdges[i];
 
@@ -28,19 +28,29 @@ public class Destroyer : MonoBehaviour {
 						GameObject endNode = edge.GetComponent<DrawEdge>().end;
 
 						GameObject otherNode = (startNode == hitObject) ? endNode : startNode;
-
 						otherNode.GetComponent<NodeController>().connectedEdges.Remove(edge);
+
 						Destroy(edge);
 					}
+					Destroy(hitObject);
+
 					for (int i = 0; i < nodeController.adjacentNodes.Count; i++) {
 						NodeController adjController = nodeController.adjacentNodes[i].GetComponent<NodeController>();
 						adjController.adjacentNodes.Remove(hitObject);
 					}
 				} else if (hitObject.tag == "Edge") {
-					
+					GameObject startNode = hitObject.GetComponent<DrawEdge>().start;
+					GameObject endNode = hitObject.GetComponent<DrawEdge>().end;
+
+					startNode.GetComponent<NodeController>().connectedEdges.Remove(hitObject);
+					endNode.GetComponent<NodeController>().connectedEdges.Remove(hitObject);
+
+					startNode.GetComponent<NodeController>().adjacentNodes.Remove(endNode);
+					endNode.GetComponent<NodeController>().adjacentNodes.Remove(startNode);
+
+					Destroy(hitObject);
 				}
-				Destroy(hit.transform.gameObject);
-				Debug.Log("I've HIT SOMETHING");
+
 			}
 		}
 	}
